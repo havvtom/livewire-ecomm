082dfc727e8893e5d573359630a798e604ed99f4
havvtom <havvtom@gmail.com>
2022-02-25 10:58:45 +0200

    indexing products in meilisearch

--- app/Http/Livewire/ProductBrowser.php
+++ app/Http/Livewire/ProductBrowser.php
@@ -10,9 +10,23 @@ class ProductBrowser extends Component
     public $category;
 
     public function render()
-    {
+    {   
+        //Search
+
+        $search = Product::search('', function ($meilisearch, string $query, array $options) {
+            // $options['filter'] = 'category_ids = ' . $this->category->id;
+
+            $options['facetsDistribution'] = ['Size', 'Color'];
+
+            return $meilisearch->search($query, $options);
+
+        })->raw();
+        
+        $products = $this->category->products->find(collect($search['hits'])->pluck('id'));
+
         return view('livewire.product-browser', [
-            'products' => Product::get()
+            'products' => $products,
+            'filters' => $search['facetsDistribution']
         ]);
     }
 }
--- resources/views/livewire/product-browser.blade.php
+++ resources/views/livewire/product-browser.blade.php
@@ -3,11 +3,13 @@
         <div class="space-y-6">
             <div class="space-y-1">
                 <ul>
-                    <li>
-                        <a href="" class="text-indigo-500">
-                            Category child
-                        </a>
-                    </li>
+                    @foreach( $category->children as $child )
+                        <li>
+                            <a href="/categories/{{$child->slug}}" class="text-indigo-500">
+                                {{ $child->title }}
+                            </a>
+                        </li>
+                    @endforeach
                 </ul>
             </div>
 
@@ -19,24 +21,30 @@
                     </div>
                 </div>
 
-                <div class="space-y-1">
-                    <div class="font-semibold">Filter title</div>
-                    <div class="flex items-center space-x-2">
-                        <input type="checkbox" id="" value=""> <label for="">Filter (count)</label>
-                    </div>
-                </div>
+                
+                    @foreach( $filters as $title => $filter )
+                        <div class="space-y-1">
+                            <div class="font-semibold">{{Str::title($title)}}</div>
+                            @foreach($filter as $option => $count)
+                                <div class="flex items-center space-x-2">
+                                    <input type="checkbox" id="" value=""> <label for="">{{$option}} ({{$count}})</label>
+                                </div>
+                            @endforeach
+                        </div>
+                    @endforeach
+                
             </div>
         </div>
     </div>
     <div class="col-span-5 sm:px-6 lg:px-8">
         <div class="mb-6">
-            Found 0 products matching your filters
+            Found {{ $products->count() }} {{ Str::plural('product', $products->count()) }} matching your filters
         </div>
 
         <div class="overflow-hidden sm:rounded-lg grid lg:grid-cols-3 md:grid-cols-2 gap-4">
             @foreach( $products as $product )
                 <a href="products/{{ $product->slug }}" class="p-6 bg-white border-b border-gray-200 space-y-4">
-                    <img src="" class="w-full">
+                    <img src="{{ $product->getFirstMediaUrl() }}" class="w-full">
 
                     <div class="space-y-1">
                         <div>{{ $product->title }}</div>
